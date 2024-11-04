a955e4eb2ea77ac12455b6145db246adf5ad2344
Salih Enes Alıcı <enesalici.tr@gmail.com>
2024-11-04 16:56:04 +0300

    added homework

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", (event) => {
+  const name = (nameOutput.textContent = event.target.value);
+
+  if (!name) {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = name;
+  }
+});
